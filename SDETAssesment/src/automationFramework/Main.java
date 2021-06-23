8001f12ac2573a49bdd49e79e8c6d7902647e253
Vikassinghmehra <vikassinghmehra@gmail.com>
2021-06-23 21:14:55 +0530

    added chrome driver

---
+++ SDETAssesment/src/automationFramework/Main.java
@@ -0,0 +1,25 @@
+package automationFramework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+	// write your code here
+        System.setProperty("webdriver.chrome.driver", "/Users/vikasmehra/Downloads/chromedriver");
+
+        //Creating WebDriver instance
+        WebDriver driver = new ChromeDriver();
+
+        //Navigate to web page
+        driver.get("https://google.com/");
+        Thread.sleep(4000);
+        driver.quit();
+
+    }
+}
