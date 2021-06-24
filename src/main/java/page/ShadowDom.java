5bcc2f113aec64f6c08d667c9a981ee667e47448
Vikassinghmehra <vikassinghmehra@gmail.com>
2021-06-24 20:06:29 +0530

    Enter Page Object Model for Q2

--- src/main/java/page/ShadowDom.java
+++ src/main/java/page/ShadowDom.java
@@ -1,4 +1,52 @@
 package page;
 
-public class ShadowDom {
+import base.TestBase;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShadowDom extends TestBase {
+
+    @FindBy(xpath = "//span[@slot='my-text']")
+    WebElement text1;
+
+    @FindBy(xpath = "//li[normalize-space()=\"Let's have some different text!\"]")
+    WebElement text2;
+
+
+
+    public ShadowDom(){
+        PageFactory.initElements(driver,this);
+    }
+
+
+    public void openUrl() {
+        try {
+            driver.get(prop.getProperty("url2"));
+            System.out.println("Url Opened");
+        }catch(Exception e){
+            System.out.println(e.getStackTrace());
+            System.out.println("Not able to Open the expected url");
+        }
+    }
+
+    public void listOfTextAndPrint() {
+        try{
+
+            List<String> list=new ArrayList<String>() ;
+            list.add(text1.getText());
+            list.add(text2.getText());
+            for(String s :list){
+                System.out.println("------------------------------------");
+                System.out.println(s);
+            }
+            System.out.println("Text Printed");
+        }catch (Exception e){
+            System.out.println(e.getStackTrace());
+            System.out.println("Text not Printed");
+        }
+    }
 }
--- src/test/java/Assignment2.java
+++ src/test/java/Assignment2.java
@@ -5,12 +5,14 @@ import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import page.ShadowDom;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Assignment2 extends TestBase {
 
+    ShadowDom shadowDom;
     public Assignment2(){
         super();
     }
@@ -18,24 +20,15 @@ public class Assignment2 extends TestBase {
     @BeforeClass
     public void setUp(){
         initialization();
+        shadowDom=new ShadowDom();
     }
 
 
     @Test(priority = 1)
     public void HomeTest() throws InterruptedException {
-        driver.get(prop.getProperty("url2"));
-        Thread.sleep(4000);
-        WebElement webElement=driver.findElement(By.xpath("//span[@slot='my-text']"));
-        List <String> list=new ArrayList<String>() ;
 
-        list.add(webElement.getText());
-        webElement=driver.findElement(By.xpath("//li[normalize-space()=\"Let's have some different text!\"]"));
-        list.add(webElement.getText());
-
-        for(String s :list){
-            System.out.println("------------------------------------");
-            System.out.println(s);
-        }
+        shadowDom.openUrl();
+        shadowDom.listOfTextAndPrint();
 
     }
 
