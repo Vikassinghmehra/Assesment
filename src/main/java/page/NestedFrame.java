41c8c65383985e370d1fb8c0c3eeeeed9f020d18
Vikassinghmehra <vikassinghmehra@gmail.com>
2021-06-24 20:22:02 +0530

    added PageObject for Q3

--- src/main/java/page/NestedFrame.java
+++ src/main/java/page/NestedFrame.java
@@ -1,5 +1,71 @@
 package page;
 
-public class NestedFrame {
+import base.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NestedFrame extends TestBase {
+
+
+    @FindBy(xpath = "//frame[@name='frame-top']")
+    WebElement top_frame;
+
+    @FindBy(xpath = "//div[@id='content']")
+    WebElement div;
+
+    @FindBy(xpath = "//frame[@name='frame-bottom']")
+    WebElement bottom_frame;
+
+    @FindBy(xpath = "//body")
+    WebElement body;
+
+
+    public NestedFrame(){
+        PageFactory.initElements(driver,this);
+    }
+
+
+    public void openUrl() {
+        try {
+            driver.get(prop.getProperty("url3"));
+            System.out.println("Url Opened");
+        }catch(Exception e){
+            System.out.println(e.getStackTrace());
+            System.out.println("Not able to Open the expected url");
+        }
+    }
+
+    public void printValuesForNestedFrame(){
+        try{
+            String []list={"frame-left","frame-middle","frame-right"};
+            for(int i=0;i<list.length;i++){
+                if(list[i]=="frame-middle"){
+                    driver.switchTo().frame(top_frame);
+                    driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='" + list[i] + "']")));
+                    System.out.println("---------------------------" + div.getText());
+                    driver.switchTo().defaultContent();
+                }else {
+                    driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='frame-top']")));
+                    driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='" + list[i] + "']")));
+                    //webElement = driver.findElement(By.xpath("//body"));
+                    System.out.println("---------------------------" + body.getText());
+                    driver.switchTo().defaultContent();
+                }
+            }
+
+            driver.switchTo().frame(bottom_frame);
+            System.out.println("---------------------------" + body.getText());
+
+
+        }catch(Exception e){
+
+        }
+    }
+
 
 }
--- src/test/java/Assignment3.java
+++ src/test/java/Assignment3.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import page.NestedFrame;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 
 public class Assignment3 extends TestBase {
 
+    NestedFrame nestedFrame;
     public Assignment3(){
         super();
     }
@@ -19,34 +21,14 @@ public class Assignment3 extends TestBase {
     @BeforeClass
     public void setUp(){
         initialization();
+        nestedFrame=new NestedFrame();
     }
 
 
     @Test(priority = 1)
     public void HomeTest() throws InterruptedException {
-        driver.get(prop.getProperty("url3"));
-        Thread.sleep(4000);
-        WebElement webElement;
-
-        String []list={"frame-left","frame-middle","frame-right"};
-        for(int i=0;i<list.length;i++){
-            if(list[i]=="frame-middle"){
-                driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='frame-top']")));
-                driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='" + list[i] + "']")));
-                webElement = driver.findElement(By.xpath("//div[@id='content']"));
-                System.out.println("---------------------------" + webElement.getText());
-                driver.switchTo().defaultContent();
-            }else {
-                driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='frame-top']")));
-                driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='" + list[i] + "']")));
-                webElement = driver.findElement(By.xpath("//body"));
-                System.out.println("---------------------------" + webElement.getText());
-                driver.switchTo().defaultContent();
-            }
-        }
-        driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='frame-bottom']")));
-        webElement = driver.findElement(By.xpath("//body"));
-        System.out.println("---------------------------" + webElement.getText());
+        nestedFrame.openUrl();
+        nestedFrame.printValuesForNestedFrame();
 
     }
 
